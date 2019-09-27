9f44a1f4925a0dc8a52d799c3088d89658c85455
grace <grace.schramka@gmail.com>
2019-09-27 17:50:25 -0500

    landing page 2.0
    revised the entirety of the index page, much more professional "coming soon" page.

--- js/custom_behaviors.js
+++ js/custom_behaviors.js
@@ -1,23 +1,38 @@
+function copyToClipboard(elementId){
+
+	var aux = document.createElement("input");
+	aux.setAttribute("value", document.getElementById(elementId).innerHTML);
+	document.body.appendChild(aux);
+	aux.select();
+	document.execCommand("copy");
+	document.body.removeChild(aux);
+
+
+	$("#alert").fadeIn(200).delay(4000).fadeOut(500)}
+
+// GLOBAL JQUERY
 $(document).ready(function() {
 
-	$(".linkedInContainer").hover(function(){
-		$(".linkedIn").css("display", "none");
-		$(".linkedInShadow").css("display", "block");
-
-	},
-	function(){
-		$(".linkedIn").css("display", "block");
-		$(".linkedInShadow").css("display", "none");
+	$('.masterTooltip').hover(function(){
+					// Hover over code
+					var title = $(this).attr('title');
+					$(this).data('tipText', title).removeAttr('title');
+					$('<p class="tooltip"></p>')
+					.text(title)
+					.appendTo('body')
+					.fadeIn('slow');
+	}, function() {
+					// Hover out code
+					$(this).attr('title', $(this).data('tipText'));
+					$('.tooltip').remove();
+	}).mousemove(function(e) {
+					var mousex = e.pageX + 20; //Get X coordinates
+					var mousey = e.pageY - 30; //Get Y coordinates
+					$('.tooltip')
+					.css({ top: mousey, left: mousex })
 	});
 
-	$(".behanceContainer").hover(function(){
-		$(".behance").css("display", "none");
-		$(".behanceShadow").css("display", "block");
+	// $("#message").on("click", function);
 
-	},
-	function(){
-		$(".behance").css("display", "block");
-		$(".behanceShadow").css("display", "none");
-	});
 
 });
